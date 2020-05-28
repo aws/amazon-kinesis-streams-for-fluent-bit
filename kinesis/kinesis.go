3ca376e9aaa7a939bd7e73c376be3256694f9809
Wesley Pettit <wppttt@amazon.com>
2020-05-28 15:21:54 -0700

    Fix bug in error processing logic

--- kinesis/kinesis.go
+++ kinesis/kinesis.go
@@ -200,6 +200,8 @@ func (outputPlugin *OutputPlugin) AddRecord(record map[interface{}]interface{}, 
 		retCode, err := outputPlugin.sendCurrentBatch()
 		if err != nil {
 			logrus.Errorf("[kinesis %d] %v\n", outputPlugin.PluginID, err)
+		}
+		if retCode != fluentbit.FLB_OK {
 			return retCode
 		}
 	}
